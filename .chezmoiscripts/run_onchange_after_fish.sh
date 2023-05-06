edf1b6e96d380b5c07928d80e16c86cb54c92fa5
nlf <quitlahok@gmail.com>
2023-05-06 11:38:03 -0700

    fix: only chsh if the shell is not already fish

--- .chezmoiscripts/run_onchange_after_fish.sh
+++ .chezmoiscripts/run_onchange_after_fish.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-if [[ -x /usr/local/bin/fish ]]; then
-  chsh -s /usr/local/bin/fish
-elif [[ -x /opt/homebrew/bin/fish ]]; then
-  chsh -s /opt/homebrew/bin/fish
+if [[ "$(basename $(dscl . -read /Users/${USER} UserShell | cut -d' ' -f2))" != "fish" ]]; then
+  if [[ -x /usr/local/bin/fish ]]; then
+    chsh -s /usr/local/bin/fish
+  elif [[ -x /opt/homebrew/bin/fish ]]; then
+    chsh -s /opt/homebrew/bin/fish
+  fi
 fi
 
 fish -c "fisher install ilancosman/tide@v5"
